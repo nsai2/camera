e4866427c212a5db2f28d90a96f8b6f024905bc4
nsai2 <nsaitis@gmail.com>
2025-08-13 17:08:44 +0300

    Delete scripts directory

--- scripts/download_model.ps1
+++
@@ -1,7 +0,0 @@
-$assetsDir = Join-Path $PSScriptRoot "..\app\src\main\assets"
-New-Item -ItemType Directory -Force -Path $assetsDir | Out-Null
-$url = "https://storage.googleapis.com/tfhub-lite-models/tensorflow/lite-model/efficientdet/lite0/detection/metadata/1.tflite"
-$out = Join-Path $assetsDir "efficientdet_lite0.tflite"
-Write-Host "Downloading EfficientDet Lite0 to $out"
-Invoke-WebRequest -Uri $url -OutFile $out
-Write-Host "Done."
